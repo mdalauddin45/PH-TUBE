6d2fbe733b374f9ad0199d6c359ad6151334da6c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-24 00:19:40 +0600

    empty page design done

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let videos = [];
+let categories = [];
+let isAscending = false;
 function formatTime(seconds) {
   return seconds < 60
     ? `${seconds} seconds ago`
@@ -11,8 +14,22 @@ function formatTime(seconds) {
         Math.floor((seconds % 3600) / 60) === 1 ? "minute" : "minutes"
       } ago`;
 }
-
-const loadCategies = () => {
+function sortViews(data, options) {
+    const sortedData = data.sort((a, b) => {
+      const aViews = parseInt(a.others.views);
+      const bViews = parseInt(b.others.views);
+      if (options) {
+        console.log(options);
+        return bViews - aViews;
+      } else {
+        console.log(options);
+        return aViews - bViews;
+      }
+    });
+    return sortedData;
+}
+  
+const loadCategories = () => {
   fetch(`https://openapi.programming-hero.com/api/videos/categories`)
     .then((res) => res.json())
     .then((data) => displyCategories(data?.data));
@@ -25,32 +42,63 @@ const displyCategories = (data) => {
     // console.log(category);
     const div = document.createElement("div");
     div.innerHTML = `
-        <button class="btn glass">${category?.category}</button>
+        <button id = ${category.category_id}  onclick="loadVideos(${category.category_id})" class="btn glass">${category?.category}</button>
         `;
     allCategory.appendChild(div);
   });
 };
 
-loadCategies();
+loadCategories();
 
 const loadVideos = (id) => {
-  fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+    fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     .then((res) => res.json())
     .then((data) => displyVideos(data.data));
 };
+const showEmpty = () => {
+  const videoContainer = document.getElementById("video-container");
+  videoContainer.innerHTML = "";
+  const emptyContainer = document.getElementById("empty");
+  const div = document.createElement("div");
+  div.innerHTML = `  <div class="flex flex-col mt-20 items-center justify-center">
+  <img src="/image/Icon.png" class="h-[140px] w-[140px]" alt="" />
+  <h2
+    class="mt-8 text-3xl text-center font-bold md:w-[400px] w-[80%] mx-auto"
+  >
+    Oops!! Sorry, There is no content here
+  </h2>
+</div>`;
+  emptyContainer.appendChild(div);
+};
 const displyVideos = (data) => {
-  // console.log(data);
-  const allVideos = document.getElementById("allVideos");
-  allVideos.innerHTML = "";
-  data.forEach((video) => {
-    // console.log(video);
-    const div = document.createElement("div");
-    div.innerHTML = `
+    const allVideos = document.getElementById("allVideos");
+    const empty = document.getElementById("empty");
+    if(data.length==0){
+        
+        allVideos.innerHTML = "";
+        empty.innerHTML = `
+        <div class="flex flex-col mt-20 items-center justify-center">
+        <img src="/image/Icon.png" class="h-[140px] w-[140px]" alt="" />
+        <h2 class="mt-8 text-3xl text-center font-bold md:w-[400px] w-[80%] mx-auto"> Oops!! Sorry, There is no content here</h2>
+      </div>
+        `;
+    }else if(data.length>1){
+        empty.innerHTML = "";
+        allVideos.innerHTML = "";
+        data.forEach((video) => {
+          // console.log(video);
+          const div = document.createElement("div");
+          div.innerHTML = `
         <div class="h-fit rounded-lg">
             <figure class="w-full min-h-[100px] h-full   sm:h-[200px] relative">
-            <img src="${video.thumbnail}" class="h-full w-full rounded-md" alt="product"/>
+            <img src="${
+              video.thumbnail
+            }" class="h-full w-full rounded-md" alt="product"/>
             <div class="absolute bottom-3 right-3 text-white text-xs bg-[#171717] p-0.5 rounded-md">
-            <span> ${video?.others?.posted_date && formatTime(video?.others?.posted_date)}</span>
+            <span> ${
+              video?.others?.posted_date &&
+              formatTime(video?.others?.posted_date)
+            }</span>
         </div>
         </figure>
         <div class="flex gap-3 mt-5">
@@ -62,17 +110,29 @@ const displyVideos = (data) => {
                 </div>
             </div>
             <div class="flex-1">
-                <h3 class="text-neutral-950 max-w-prose line-clamp-2 font-bold text-base ">${video.title}</h3>
+                <h3 class="text-neutral-950 max-w-prose line-clamp-2 font-bold text-base ">${
+                  video.title
+                }</h3>
                 <div class="flex gap-2  mt-2 mb-2 items-center">
-                    <p class="text-sm text-heroText">${video?.authors[0].profile_name}</p>
-                    ${video?.authors[0].verified ? '<img src="/Image/verified.svg" alt="" />' : ""}
+                    <p class="text-sm text-heroText">${
+                      video?.authors[0].profile_name
+                    }</p>
+                    ${
+                      video?.authors[0].verified
+                        ? '<img src="/Image/verified.svg" alt="" />'
+                        : ""
+                    }
                 </div>
-                <p class="text-sm mt-2 text-heroText">${video?.others?.views} views</p>
+                <p class="text-sm mt-2 text-heroText">${
+                  video?.others?.views
+                } views</p>
             </div>
             </div>
         </div>
         `;
-    allVideos.appendChild(div);
-  });
+          allVideos.appendChild(div);
+        });
+    }
 };
-loadVideos(1000);
+loadVideos(1000)
+loadCategories();
